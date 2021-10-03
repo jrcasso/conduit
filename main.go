1faa2bcfbaf04eca49df8712eaad5e0eb61cf50e
Justin Casso <justincasso1@gmail.com>
2021-10-03 22:07:47 +0000

    Add initial transform package and usage

--- main.go
+++ main.go
@@ -1,107 +1,21 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
-	"sync"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/jrcasso/transformer/transform"
 )
 
-// {
-// 	"Records":[
-// 		 {
-// 				"eventVersion":"2.1",
-// 				"eventSource":"aws:s3",
-// 				"awsRegion":"us-east-1",
-// 				"eventTime":"2021-10-03T05:05:03.622Z",
-// 				"eventName":"ObjectCreated:Put",
-// 				"userIdentity":{  // Not implemented
-// 					 "principalId":"AIDAJDPLRKLG7UEXAMPLE"
-// 				},
-// 				"requestParameters":{  // Not implemented
-// 					 "sourceIPAddress":"127.0.0.1"
-// 				},
-// 				"responseElements":{  // Not implemented
-// 					 "x-amz-request-id":"27e06596",
-// 					 "x-amz-id-2":"eftixk72aD6Ap51TnqcoF8eFidJG9Z/2"
-// 				},
-// 				"s3":{
-// 					 "s3SchemaVersion":"1.0",  // Not implemented
-// 					 "configurationId":"testConfigRule",  // Not implemented
-// 					 "bucket":{
-// 							"name":"ingress",
-// 							"ownerIdentity":{  // Not implemented
-// 								 "principalId":"A3NL1KOZZKExample"
-// 							},
-// 							"arn":"arn:aws:s3:::ingress"
-// 					 },
-// 					 "object":{
-// 							"key":"Makefile",
-// 							"size":59,
-// 							"eTag":"2d21a73d66fe9a154b3e7e1442e82c1c",
-// 							"versionId":null,
-// 							"sequencer":"0055AED6DCD90281E5"
-// 					 }
-// 				}
-// 		 }
-// 	]
-// }
-
-type Response struct {
-	Records []Record `json:"Records"`
-}
-
-type Record struct {
-	EventVersion string   `json:"eventVersion"`
-	EventSource  string   `json:"eventSource"`
-	EwsRegion    string   `json:"awsRegion"`
-	EventTime    string   `json:"eventTime"`
-	EventName    string   `json:"eventName"`
-	S3           S3Record `json:"s3"`
-}
-
-type S3Record struct {
-	Bucket S3BucketRecord `json:"bucket"`
-	Object S3ObjectRecord `json:"object"`
-}
-
-type S3ObjectRecord struct {
-	ETag      string `json:"eTag"`
-	Key       string `json:"key"`
-	Size      int    `json:"size"`
-	Sequencer string `json:"sequencer"`
-}
-
-type S3BucketRecord struct {
-	Name string
-	Arn  string
-}
-
-type Upload struct {
-	Bucket string
-	Key    string
-	Data   string
-}
-
 func main() {
-	var visibility, batchSize int64
-	var s3Ingress, s3Egress, queueUrl string
-	var response Response
-
-	s3Ingress = os.Getenv("TRANSFORM_S3_INGRESS_BUCKET")
-	s3Egress = os.Getenv("TRANSFORM_S3_EGRESS_BUCKET")
-	queueUrl = os.Getenv("TRANSFORM_QUEUE_URL")
-	visibility, _ = strconv.ParseInt(os.Getenv("TRANSFORM_VISIBILITY_TIMEOUT"), 10, 64)
-	batchSize, _ = strconv.ParseInt(os.Getenv("TRANSFORM_BATCH_SIZE"), 10, 64)
-
+	ctx, cancel := context.WithCancel(context.Background())
+	visibility, _ := strconv.ParseInt(os.Getenv("TRANSFORM_VISIBILITY_TIMEOUT"), 10, 64)
+	batchSize, _ := strconv.ParseInt(os.Getenv("TRANSFORM_BATCH_SIZE"), 10, 64)
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		Config: aws.Config{
 			S3ForcePathStyle: aws.Bool(true),
@@ -110,106 +24,49 @@ func main() {
 		},
 		SharedConfigState: session.SharedConfigEnable,
 	}))
-
-	f, _ := os.Open("go.mod")
-	uploader := s3manager.NewUploader(sess)
-	result, err := uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(s3Ingress),
-		Key:    aws.String("foo"),
-		Body:   f,
-	})
-	if err != nil {
-		panic(fmt.Sprintf("%+v", err))
-	}
-	fmt.Println(result)
-
-	svc := sqs.New(sess)
-	messages, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
-		AttributeNames: []*string{
-			aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
-		},
-		MessageAttributeNames: []*string{
-			aws.String(sqs.QueueAttributeNameAll),
-		},
-		QueueUrl:            aws.String(queueUrl),
-		MaxNumberOfMessages: &batchSize,
-		VisibilityTimeout:   &visibility,
-	})
-	if err != nil {
-		panic(fmt.Sprintf("%+v", err))
-	}
-	fmt.Printf("%+v", messages)
-	for _, message := range messages.Messages {
-		fmt.Println(message.Body)
-		json.Unmarshal([]byte(*message.Body), &response)
-		fmt.Println(response)
-	}
-	downloader := s3manager.NewDownloader(sess)
-	downloadChannel := make(chan string)
-	uploadChannel := make(chan string)
-	var wg sync.WaitGroup
-	for _, record := range response.Records {
-		wg.Add(1)
-		go func(record Record, wg *sync.WaitGroup) {
-			defer wg.Done()
-
-			fmt.Println("Downloading...")
-			buff := &aws.WriteAtBuffer{}
-			_, err := downloader.Download(buff, &s3.GetObjectInput{
-				Bucket: &record.S3.Bucket.Name,
-				Key:    &record.S3.Object.Key,
-			})
-			if err != nil {
-				panic(fmt.Sprintf("%+v", err))
+	t := transform.Transformer{
+		S3Ingress:  os.Getenv("TRANSFORM_S3_INGRESS_BUCKET"),
+		S3Egress:   os.Getenv("TRANSFORM_S3_EGRESS_BUCKET"),
+		QueueUrl:   os.Getenv("TRANSFORM_QUEUE_URL"),
+		Visibility: visibility,
+		BatchSize:  batchSize,
+		Session:    *sess,
+		Transform: func(t transform.Transformable, c chan<- transform.Upload) {
+			fmt.Println("Transforming record...")
+			c <- transform.Upload{
+				Data: fmt.Sprintf("FOO %v", t.Data),
+				Key:  "test-2",
 			}
-
-			data := string(buff.Bytes())
-			fmt.Printf("%+v", data)
-			downloadChannel <- data
-			fmt.Println("Downloaded")
-		}(record, &wg)
-
-		wg.Add(1)
-		go func(wg *sync.WaitGroup) {
-			defer wg.Done()
-
-			fmt.Println("Transforming...")
-			transformedData := transform(<-downloadChannel)
-			fmt.Println("Transformed")
-			fmt.Printf("%+v", transformedData)
-			uploadChannel <- transformedData
-		}(&wg)
-
-		wg.Add(1)
-		go func(wg *sync.WaitGroup) {
-			defer wg.Done()
-
-			fmt.Println("Uploading...")
-			_, err := uploader.Upload(&s3manager.UploadInput{
-				Bucket: aws.String(s3Egress),
-				Key:    aws.String("foo"),
-				Body:   strings.NewReader(<-uploadChannel),
-			})
-			if err != nil {
-				panic(fmt.Sprintf("%+v", err))
-			}
-			fmt.Println("Uploaded")
-		}(&wg)
+			fmt.Println("Transformed record!")
+		},
 	}
-	wg.Wait()
+
+	defer func() {
+		cancel()
+	}()
+
+	if err := run(ctx, t); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
 }
 
-func transform(data string) string {
-	return fmt.Sprintf("FOO %s", data)
+func run(ctx context.Context, t transform.Transformer) error {
+	downloadQueue := make(chan transform.Record)
+	transformQueue := make(chan transform.Transformable)
+	uploadQueue := make(chan transform.Upload)
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-time.Tick(5 * time.Second):
+			go t.Receive(downloadQueue)
+		case record := <-downloadQueue:
+			go t.Download(record, transformQueue)
+		case data := <-transformQueue:
+			go t.Transform(data, uploadQueue)
+		case upload := <-uploadQueue:
+			go t.Upload(upload)
+		}
+	}
 }
-
-// func upload(uploader s3manager.Uploader, uploadChannel <-chan string, wg *sync.WaitGroup) {
-// 	_, err := uploader.Upload(&s3manager.UploadInput{
-// 		Bucket: aws.String(s3Egress),
-// 		Key:    aws.String("foo"),
-// 		Body:   strings.NewReader(<-uploadChannel),
-// 	})
-// 	if err != nil {
-// 		panic(fmt.Sprintf("%+v", err))
-// 	}
-// }
--- transform/transform.go
+++ transform/transform.go
@@ -1,7 +1,177 @@
 package transform
 
-type Transformer interface {
-	Transform(Data string) string
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+// {
+// 	"Records":[
+// 		 {
+// 				"eventVersion":"2.1",
+// 				"eventSource":"aws:s3",
+// 				"awsRegion":"us-east-1",
+// 				"eventTime":"2021-10-03T05:05:03.622Z",
+// 				"eventName":"ObjectCreated:Put",
+// 				"userIdentity":{  // Not implemented
+// 					 "principalId":"AIDAJDPLRKLG7UEXAMPLE"
+// 				},
+// 				"requestParameters":{  // Not implemented
+// 					 "sourceIPAddress":"127.0.0.1"
+// 				},
+// 				"responseElements":{  // Not implemented
+// 					 "x-amz-request-id":"27e06596",
+// 					 "x-amz-id-2":"eftixk72aD6Ap51TnqcoF8eFidJG9Z/2"
+// 				},
+// 				"s3":{
+// 					 "s3SchemaVersion":"1.0",  // Not implemented
+// 					 "configurationId":"testConfigRule",  // Not implemented
+// 					 "bucket":{
+// 							"name":"ingress",
+// 							"ownerIdentity":{  // Not implemented
+// 								 "principalId":"A3NL1KOZZKExample"
+// 							},
+// 							"arn":"arn:aws:s3:::ingress"
+// 					 },
+// 					 "object":{
+// 							"key":"Makefile",
+// 							"size":59,
+// 							"eTag":"2d21a73d66fe9a154b3e7e1442e82c1c",
+// 							"versionId":null,
+// 							"sequencer":"0055AED6DCD90281E5"
+// 					 }
+// 				}
+// 		 }
+// 	]
+// }
+
+type Transformer struct {
+	S3Ingress  string
+	S3Egress   string
+	QueueUrl   string
+	Visibility int64
+	BatchSize  int64
+	Session    session.Session
+	Transform  func(Transformable, chan<- Upload)
 }
 
-type Transform interface{}
+type Config struct {
+	S3Ingress  string
+	S3Egress   string
+	QueueUrl   string
+	Visibility int64
+	BatchSize  int64
+}
+
+type Response struct {
+	Records []Record `json:"Records"`
+}
+
+type Record struct {
+	EventVersion string   `json:"eventVersion"`
+	EventSource  string   `json:"eventSource"`
+	EwsRegion    string   `json:"awsRegion"`
+	EventTime    string   `json:"eventTime"`
+	EventName    string   `json:"eventName"`
+	S3           S3Record `json:"s3"`
+}
+
+type S3Record struct {
+	Bucket S3BucketRecord `json:"bucket"`
+	Object S3ObjectRecord `json:"object"`
+}
+
+type S3ObjectRecord struct {
+	ETag      string `json:"eTag"`
+	Key       string `json:"key"`
+	Size      int    `json:"size"`
+	Sequencer string `json:"sequencer"`
+}
+
+type S3BucketRecord struct {
+	Name string
+	Arn  string
+}
+
+type Transformable struct {
+	Record Record
+	Data   string
+}
+
+type Upload struct {
+	Key  string
+	Data string
+}
+
+func (t Transformer) Download(record Record, ch chan<- Transformable) {
+	fmt.Println("Downloading record...")
+	downloader := s3manager.NewDownloader(&t.Session)
+	buff := &aws.WriteAtBuffer{}
+	_, err := downloader.Download(buff, &s3.GetObjectInput{
+		Bucket: &record.S3.Bucket.Name,
+		Key:    &record.S3.Object.Key,
+	})
+
+	if err != nil {
+		panic(fmt.Sprintf("%+v", err))
+	}
+	fmt.Println("Downloaded record!")
+	ch <- Transformable{
+		Record: record,
+		Data:   string(buff.Bytes()),
+	}
+}
+
+func (t Transformer) Receive(ch chan<- Record) {
+	var response Response
+	svc := sqs.New(&t.Session)
+	fmt.Println("Receiving record...")
+	messages, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+		AttributeNames: []*string{
+			aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
+		},
+		MessageAttributeNames: []*string{
+			aws.String(sqs.QueueAttributeNameAll),
+		},
+		QueueUrl:            aws.String(t.QueueUrl),
+		MaxNumberOfMessages: &t.BatchSize,
+		VisibilityTimeout:   &t.Visibility,
+	})
+	if err != nil {
+		panic(fmt.Sprintf("%+v", err))
+	}
+	fmt.Println("Received record!")
+	for _, message := range messages.Messages {
+		json.Unmarshal([]byte(*message.Body), &response)
+	}
+	if len(response.Records) > 0 {
+		for _, record := range response.Records {
+			fmt.Println("Adding record to queue...")
+			ch <- record
+			fmt.Println("Added record to queue")
+		}
+	} else {
+		fmt.Println("No messages received.")
+	}
+}
+
+func (t Transformer) Upload(upload Upload) {
+	fmt.Println("Uploading record...")
+	uploader := s3manager.NewUploader(&t.Session)
+	_, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(t.S3Egress),
+		Key:    aws.String(upload.Key),
+		Body:   strings.NewReader(upload.Data),
+	})
+	if err != nil {
+		panic(fmt.Sprintf("%+v", err))
+	}
+	fmt.Println("Uploaded record!")
+}
